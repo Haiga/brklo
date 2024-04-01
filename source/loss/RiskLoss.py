c25887ad0b729e49c81fd6504ed635f076640009
pedro.rodrigues <pedro.rodrigues@netbr.com.br>
2024-04-01 10:19:19 -0300

    teste

--- source/loss/RiskLoss.py
+++ source/loss/RiskLoss.py
@@ -1,13 +1,20 @@
+from pytorch_metric_learning.losses import TripletMarginLoss
 from torch import nn
+from source.miner.RelevanceMiner import RelevanceMiner
+from pytorch_metric_learning.utils import common_functions as c_f
 
 
 class RiskLoss(nn.Module):
 
     def __init__(self, params):
-        pass
+        c_f.check_shapes = lambda x, y: None
+        super(RiskLoss, self).__init__()
+        self.miner = RelevanceMiner(params.miner)
+        self.criterion = TripletMarginLoss()
 
     def forward(self, query_idx, query_rpr, passage_idx, passage_rpr):
         print("---------------------------")
         print(query_idx, query_rpr, passage_idx, passage_rpr)
         print("----------------------flag-------------")
-        pass
+        miner_outs = self.miner.mine(query_idx=query_idx, passage_idx=passage_idx)
+        return self.criterion(query_rpr, None, miner_outs, passage_rpr, None)
